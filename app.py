c2f8c89b2a52a9cdd7db8f3adaa94bb496fcb51a
RoboVedh <89797345+RoboVedh@users.noreply.github.com>
2021-09-07 20:21:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,9 +14,9 @@ auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 print("Authentication approved")
 while True:
-  user = api.get_user('@Ameen91741779')
+  user = api.get_user('@RoboVedh')
   u = user.followers_count
-  api.update_profile(name=f'AMEER {u} Followers')
-  print(f'AMEER {u} Followers')
+  api.update_profile(name=f'@RoboVedh {u} Followers')
+  print(f'@RoboVedh {u} Followers')
   time.sleep(60)
  
